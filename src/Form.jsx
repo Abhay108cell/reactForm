b4ba395c4f882350b4bb0d2ff4feb149509953bf
abhay kumar <abhaykumar2871999@gmail.com>
2024-04-29 23:22:07 +0530

    value upgrade

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react"
+
 export default function Form(){
+  let [fullName, setFullName] = useState("")
   return(
     <form>
-      <input placeholder="text" />
+      <input placeholder="enter your name" type="text"  value={fullName}/>
       <br />
       <button>Submit</button>
     </form>
