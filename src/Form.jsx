539897a5260071a68cf6f995adc019ea0cf12c8f
abhay kumar <abhaykumar2871999@gmail.com>
2024-04-30 16:21:58 +0530

    adding event in component

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,9 +2,16 @@ import { useState } from "react"
 
 export default function Form(){
   let [fullName, setFullName] = useState("")
+
+  let handleNameChange = (e) =>{
+    console.log(e.target);
+  } 
+  
   return(
     <form>
-      <input placeholder="enter your name" type="text"  value={fullName}/>
+      <input placeholder="enter your name" type="text"  value={fullName} onChange={handleNameChange}/>
+      <br />
+      <br />
       <br />
       <button>Submit</button>
     </form>
