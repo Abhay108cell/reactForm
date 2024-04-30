45988a2718e3fff4ae396be36575235b12e2c8c0
abhay kumar <abhaykumar2871999@gmail.com>
2024-04-30 17:23:23 +0530

    adding another useState for userName

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,20 +2,24 @@ import { useState } from "react"
 
 export default function Form(){
   let [fullName, setFullName] = useState("")
+  let [userName, setUserName] = useState("")
 
   let handleNameChange = (e) =>{
     setFullName(e.target.value);
   } 
+  let handleUserName = (e) =>{
+    setUserName(e.target.value);
+  } 
   
   return(
     <form>
-      <label htmlFor="userName">Full Name</label> 
+      <label htmlFor="fullName">Full Name</label> 
       <br />
-      <input placeholder="enter your name" type="text"  value={fullName} onChange={handleNameChange}  id="username"/><br/>
+      <input placeholder="enter your name" type="text"  value={fullName} onChange={handleNameChange}  id="fullName"/><br/>
       <br />
       <label htmlFor="username">user Name</label> 
       <br />
-      <input placeholder="enter username" type="text"  value={fullName} onChange={handleNameChange}  id="username"/><br/>
+      <input placeholder="enter username" type="text"  value={userName} onChange={handleUserName}  id="username"/><br/>
       <br />
       <br />
       <br />
