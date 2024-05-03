0ff3d97f2902c49337b279e9b9aa4ce001c46103
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-03 16:43:47 +0530

    using useEffect function in comp

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function Counter(){
   let [count, setCount] = useState(0);
   let inCount = ()=>{
     setCount (c => c +1)
   }
+
+  useEffect(function printSomething(){
+    console.log("this is our side effect");
+  })
+
   return(
     <div>
       <h3 >Count = {count}</h3>
-      <button onClick={inCount}>increase</button>
+      <button onClick={inCount}>increase + 1</button>
     </div>
   )
 }
