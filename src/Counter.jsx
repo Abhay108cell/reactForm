4d5fe62b2757487882bde47fe6127390007017dd
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-03 22:38:59 +0530

    using useEffect function in comp with dependencies

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,18 +1,24 @@
 import { useState, useEffect } from "react";
 export default function Counter(){
-  let [count, setCount] = useState(0);
-  let inCount = ()=>{
-    setCount (c => c +1)
+  let [countx, setCountx] = useState(0);
+  let [county, setCounty] = useState(0);
+  let inCountx = ()=>{
+    setCountx (c => c +1)
+  }
+  let inCounty = ()=>{
+    setCounty (c => c +1)
   }
 
   useEffect(function printSomething(){
     console.log("this is our side effect");
-  })
+  },[])
 
   return(
     <div>
-      <h3 >Count = {count}</h3>
-      <button onClick={inCount}>increase + 1</button>
+      <h3 >Count = {countx}</h3>
+      <button onClick={inCountx}>increase + 1</button>
+      <h3 >Count = {county}</h3>
+      <button onClick={inCounty}>increase + 1</button>
     </div>
   )
 }
